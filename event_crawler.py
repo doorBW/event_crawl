e1fed1b6c36b7aed9b74506ffb8fbed8408ede28
doorBW <doorbw@outlook.com>
2019-02-10 22:01:37 +0900

    logic finish

--- event_crawler.py
+++ event_crawler.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 from email.mime.text import MIMEText
+from datetime import datetime
 
 #load env
 import os
@@ -16,11 +17,34 @@ load_dotenv(dotenv_path)
 status = os.getenv('STATUS')
 print(status)
 
-# for crawl
+mail_html = "<html><head><body>"+datetime.today().strftime("%Y/%m/%d %H:%M:%S")+"<br>"  # YYYY/mm/dd HH:MM:SS 형태의 시간 출력
+
+mail_html += "<h1>NEWS</h1>"
+# for news crawl
+req = requests.get("https://okky.kr/articles/news")
+html = req.text
+soup = BeautifulSoup(html, 'html.parser')
+news_titles = soup.select('div#list-article > div.panel > ul > li > div > h5 > a')
+news_titles_date = soup.select('div#list-article > div.panel > ul > li > div.list-group-item-author > div > div > div.date-created > span')
+for i in range(5):
+    mail_html += "<h4><a href='https://okky.kr"+news_titles[i].get('href')+"'>"+news_titles[i].text.strip()+"</a> _ "+news_titles_date[i].text.strip()+"</h4>"
+mail_html += "<h3><a href='https://okky.kr/articles/news'>뉴스 관련글 모두 보기</a></h3><br><br>"
+
+
+mail_html += "<h1>EVENTS</h1>"
+# for event crawl
 req = requests.get("https://okky.kr/articles/event")
 html = req.text
 soup = BeautifulSoup(html, 'html.parser')
-event_titles = soup.select('ul > li > div > h5')
+event_titles = soup.select('div#list-article > div.panel > ul > li > div > h5 > a')
+event_titles_date = soup.select('div#list-article > div.panel > ul > li > div.list-group-item-author > div > div > div.date-created > span')
+for i in range(3):
+    mail_html += "<h4><a href='https://okky.kr"+event_titles[i].get('href')+"'>"+event_titles[i].text.strip()+"</a> _ "+event_titles_date[i].text.strip()+"</h4>"
+mail_html += "<h3><a href='https://okky.kr/articles/event'>이벤트 관련글 모두 보기</a></h3>"
+
+
+mail_html += "</body></head></html>"
+print(mail_html)
 
 # open SMTP
 smtp = smtplib.SMTP('smtp.gmail.com', 587)
@@ -28,17 +52,16 @@ smtp.ehlo()      # say Hello
 smtp.starttls()  # TLS 사용시 필요
 smtp.login('doobw@likelion.org', os.getenv('PASSWORD'))
  
-# main text
-msg = MIMEText('본문 테스트 메시지')
+# main html
+msg = MIMEText(mail_html,'html')
 # title
-msg['Subject'] = '테스트'
+msg['Subject'] = 'OKKY 커뮤니티 News & Event 상위 글'
+# from
+msg['From'] = os.getenv("FROM")
 # to
-msg['To'] = 'qjadn9@naver.com'
+msg['To'] = os.getenv("TO")
 # from / to / msg
-smtp.sendmail('doobw@likelion.org', 'qjadn9@naver.com', msg.as_string())
+smtp.sendmail(msg['From'], msg['To'].split(','), msg.as_string())
  
 smtp.quit()
 
-
-# for title in event_titles:
-#     print(title.text)
